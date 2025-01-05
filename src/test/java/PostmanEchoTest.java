172ba83955d15220800f86aebb91d2a210b56428
Anastasia Zef <zeff1983@gmail.com>
2025-01-05 18:54:22 +0300

    Build Successful commit

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,5 +1,26 @@
 package ru.netology;
 
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnPostRequestBody() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello world") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello world"))
+        ;
+    }
 }
